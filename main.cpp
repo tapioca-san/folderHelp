0d416490f21d9765b4a0852dcef16233b13c42b0
Pipebomb <150746858+tapioca-san@users.noreply.github.com>
2024-12-19 18:45:23 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "config.hpp"
-#include <filesystem>
+#include <algorithm>
+#include <cctype>
 
 
 namespace fs = std::filesystem;
@@ -8,17 +9,11 @@ int main(){
 
     fs::path currentPath = fs::current_path();
     fs::path destinationFolderImage = currentPath / "image";
-
     fs::path destinationFolderVideo = currentPath / "video";
-
     fs::path destinationFolderProgramWin = currentPath / "programWindow";
-
     fs::path destinationFolderTxt = currentPath / "txt";
-
     fs::path destinationFolderZip = currentPath / "zip";
-
     fs::path destinationFolderIso = currentPath / "iso";
-    
 
     std::unordered_map<std::string, std::string> commands = {
         {".png",  destinationFolderImage  },
@@ -41,26 +36,24 @@ int main(){
         {".rar",  destinationFolderZip  },
         {".tar",  destinationFolderZip  },
 
-        
         {".iso",  destinationFolderIso  },
-
-
     };
 
 
     for (const auto& entry : fs::directory_iterator(currentPath)) {
         for (const auto& [command, pathToFolderRight] : commands) {
-            if(entry.path().extension() == command){
-                
+
+            std::string commandUp = command;
+            std::transform(commandUp.begin(), commandUp.end(), commandUp.begin(), [](unsigned char c) { return std::toupper(c); });
+
+            if(entry.path().extension() == command || entry.path().extension() == commandUp){
                 if (!fs::exists(pathToFolderRight)) {
                     fs::create_directory(pathToFolderRight);
                 }
-                
                 fs::path newLocation = pathToFolderRight / entry.path().filename();
                 fs::rename(entry.path(),   newLocation );
 
-                
-                
+                std::cout << entry.path().filename() << " moved to " << pathToFolderRight << "\n";
             }
         }
     }
